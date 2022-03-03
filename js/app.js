74a67a409ffef37d702bf0adfeb985aa663e2358
naeem <kazinaeemrayhan@gmail.com>
2022-03-03 19:49:04 +0600

    api added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 // api key
 const API_KEY = "aae42156c5a7bdd8a155af712e5e4939";
 
-const searchTemperature = () => {};
+const API = async (cityName) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  display(data);
+};
+
+const searchTemperature = () => {
+  const inputCityName = document.getElementById("input-city-name");
+  const cityName = inputCityName.value;
+  API(cityName);
+  inputCityName.value = "";
+};
+
+const display = (data) => {
+  console.log(data);
+};
