e68264afc1fd7d0d030eabd6f8bec2fbbf3a2ae0
naeem <kazinaeemrayhan@gmail.com>
2022-03-03 20:18:08 +0600

    done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const API_KEY = "aae42156c5a7bdd8a155af712e5e4939";
 
 const API = async (cityName) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
   display(data);
@@ -16,5 +16,14 @@ const searchTemperature = () => {
 };
 
 const display = (data) => {
-  console.log(data);
+  //   console.log(data);
+  const weatherLogo = document.getElementById("weather-logo");
+  const logoUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  weatherLogo.setAttribute("src", logoUrl);
+  const location = document.getElementById("location");
+  location.innerText = data.name;
+  const temp = document.getElementById("temp");
+  temp.innerText = data.main.temp;
+  const weather = document.getElementById("weather");
+  weather.innerText = data.weather[0].main;
 };
